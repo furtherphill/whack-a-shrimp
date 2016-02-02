944f113e816e3399c6525bfb6d6de72d49e8621f
furtherphill <phillip.pasqual@gmail.com>
2016-02-03 00:42:11 +0800

    file to control the motors (just a test)

---
+++ assets/js/motorSliders.js
@@ -0,0 +1,14 @@
+$(function() {
+	var numMotors = 4;
+alert("test");
+    /*for(var i = 0; i < numMotors; i++){
+    	var newSlider = $( this ).empty().slider({
+        value: 25,
+        range: "min",
+        animate: true,
+        orientation: "vertical"
+      });
+    	alert(i);
+    });
+    }*/
+});
